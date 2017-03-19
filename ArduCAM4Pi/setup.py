c7d32ab7cdff0ad1be655b08e160789246276a70
Prosper <oprospero@user.noreply.github.com>
2017-03-18 17:28:01 -0700

    Initial commit

---
+++ ArduCAM4Pi/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup, Extension
+
+module1 = Extension('demo',
+                    include_dirs = [''],
+                    libraries = [''],
+                    sources = ['demo.c'])
+
+setup (name = 'PackageName',
+       version = '1.0',
+       description = 'This is a demo package',
+       author = 'Martin v. Loewis',
+       author_email = 'martin@v.loewis.de',
+       url = 'https://docs.python.org/extending/building',
+       long_description = '''
+This is really just a demo package.
+''',
+       ext_modules = [module1])
