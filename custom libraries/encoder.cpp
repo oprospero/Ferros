b67355b015844415f7ed9faf236633b294a7e053
Prosper <oprospero@user.noreply.github.com>
2017-03-23 19:52:23 -0700

    Rename files

---
+++ custom libraries/encoder.cpp
@@ -0,0 +1,46 @@
+
+#include "encoder.h"
+#include "pindef.h"
+
+Encoder::Encoder() :
+    lastTimeLeftEncoder(0),
+    lastTimeRightEncoder(0),
+    diffLeft(0),
+    diffRight(0)
+{
+    pinMode(PIN_ENCODER_1, INPUT);
+    pinMode(PIN_ENCODER_2, INPUT);
+}
+
+void Encoder::begin()
+{
+    attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_1), ISR_getLeftEncoderTime, RISING);
+    attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_2), ISR_getRightEncoderTime, RISING);
+}
+
+float Encoder::getLeftSpeed()
+{
+    unsigned long currentTime = micros();
+    unsigned long diff = currentTime - lastTimeLeftEncoder;
+    if ()
+    return 1.0 / diffLeft;
+}
+
+float Encoder::gethRightSpeed()
+{
+    return 1.0 / diffRight;
+}
+
+void Encoder::ISR_getLeftEncoderTime()
+{
+    unsigned long currentTime = micros();
+    diffLeft = currentTime - lastTimeLeftEncoder;
+    lastTimeLeftEncoder = currentTime;
+}
+
+void Encoder::ISR_getRightEncoderTime()
+{
+    unsigned long currentTime = micros();
+    diffRight = currentTime - lastTimeRightEncoder;
+    lastTimeRightEncoder = currentTime
+}
