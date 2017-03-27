8e9728b298384a766d6b2afa8c3df1bf162b745e
Prosper <vanprosper@gmail.com>
2017-03-26 22:56:40 -0700

    Testing vex servo

---
+++ VexMotorTest/VexMotorTest.ino
@@ -0,0 +1,18 @@
+ #define PIN_PWM 9
+ 
+ void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(PIN_PWM, OUTPUT);
+  Serial.println("Ready Player One");
+}
+
+void loop() {
+
+  if (Serial.available())
+  {
+    int num = Serial.parseInt();
+    analogWrite(PIN_PWM, num);
+    Serial.println(num);
+  }
+}
