732c2e4328cd939fb8000872ee0eda439d862997
Prosper <vanprosper@gmail.com>
2017-03-26 22:54:27 -0700

    Update encoder interupt library
    Interrupt function now just triggers a flag but will still rely on frequency polling to get speed

--- custom libraries/encoderInterrupt.cpp
+++ custom libraries/encoderInterrupt.cpp
@@ -1,17 +1,21 @@
 
-#include "encoder.h"
+#include "encoderInterrupt.h"
 #include "pindef.h"
 
 Encoder::Encoder() :
     leftTrigger(false),
-    rightTrigger(false)
+    rightTrigger(false),
+    lastTimeLeft(0),
+    lastTimeRight(0),
+    leftSpeed(0),
+    rightSpeed(0)
 {
-    pinMode(PIN_ENCODER_1, INPUT);
-    pinMode(PIN_ENCODER_2, INPUT);
 }
 
 void Encoder::begin()
 {
+    pinMode(PIN_ENCODER_1, INPUT);
+    pinMode(PIN_ENCODER_2, INPUT);
     attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_1), ISR_getLeftEncoderTime, RISING);
     attachInterrupt(digitalPinToInterrupt(PIN_ENCODER_2), ISR_getRightEncoderTime, RISING);
 }
@@ -19,26 +23,53 @@ void Encoder::begin()
 float Encoder::getLeftSpeed()
 {
     unsigned long currentTime = micros();
-    unsigned long diff = currentTime - lastTimeLeftEncoder;
-    if ()
-    return 1.0 / diffLeft;
+    unsigned long diff = currentTime - lastTimeLeft;
+    if (leftTrigger)
+    {
+        speed = 1.0 / diff;
+        lastTimeLeft = currentTime;
+        leftTrigger = false;
+    }
+    else if (diff < SPEED_TIMEOUT)
+    {
+        speed = speed / 2.0;
+    }
+    else
+    {
+        speed = 0;
+    }
+    
+    return speed;
 }
 
-float Encoder::gethRightSpeed()
+float Encoder::getRightSpeed()
 {
-    return 1.0 / diffRight;
+    unsigned long currentTime = micros();
+    unsigned long diff = currentTime - lastTimeRight;
+    if (rightTrigger)
+    {
+        speed = 1.0 / diff;
+        lastTimeRight = currentTime;
+        rightTrigger = false;
+    }
+    else if (diff < SPEED_TIMEOUT)
+    {
+        speed = speed / 2.0;
+    }
+    else
+    {
+        speed = 0;
+    }
+    
+    return speed;
 }
 
 void Encoder::ISR_getLeftEncoderTime()
 {
-    unsigned long currentTime = micros();
-    diffLeft = currentTime - lastTimeLeftEncoder;
-    lastTimeLeftEncoder = currentTime;
+    leftTrigger = true;
 }
 
 void Encoder::ISR_getRightEncoderTime()
 {
-    unsigned long currentTime = micros();
-    diffRight = currentTime - lastTimeRightEncoder;
-    lastTimeRightEncoder = currentTime
+    rightTrigger = true;
 }
--- custom libraries/encoderInterrupt.h
+++ custom libraries/encoderInterrupt.h
@@ -2,19 +2,24 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 
+#define SPEED_TIMEOUT 50000
 
 class Encoder
 {
 	volatile bool leftTrigger;
 	volatile bool rightTrigger;
-
+	unsigned long lastTimeLeft;
+	unsigned long lastTimeRight;
+	float leftSpeed;
+	float rightSpeed;
 
 	Encoder();
 	void begin();
-	float getLeftSpeed();
-	float getRightSpeed();
 
     void pollSpeed();
+
+	float getLeftSpeed();
+	float getRightSpeed();
 
 	ISR_getLeftEncoderTime();
 	ISR_getRightEncoderTime();
