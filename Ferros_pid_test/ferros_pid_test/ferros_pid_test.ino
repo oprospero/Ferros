dad72f8ef3c7e8f7ea651a79a8e050a42e4062d7
Likhi <likhi.ondov@gmail.com>
2017-03-28 19:27:46 -0700

    Added motor.h to pid test code folder

--- Ferros_pid_test/ferros_pid_test/ferros_pid_test.ino
+++ Ferros_pid_test/ferros_pid_test/ferros_pid_test.ino
@@ -1,23 +1,35 @@
 #include <PID_v1.h>
 #include "encoderInterrupt.h"
+#include "motor.h"
 
 #define PIN_INPUT 0
 #define PIN_OUTPUT 3
 
-unsigned int throttle = 0;
+unsigned int throttle;
 
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
 //Specify the links and initial tuning parameters
-double Kp=2, Ki=5, Kd=1;
+double Kp = 2, Ki = 5, Kd = 1;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+
+Encoder encoder;
+Motor motor;
+
+unsigned long lastTime = 0;
 
 void setup()
 {
+
+  throttle = 0;
+  encoder.begin();
+  motor.begin();
+  Serial.begin(115200);
+
   //initialize the variables we're linked to
-  Input = analogRead(PIN_INPUT);
-  Setpoint = 100;
+  Input = 0;
+  Setpoint = 0;
 
   //turn the PID on
   myPID.SetMode(AUTOMATIC);
@@ -25,7 +37,18 @@ void setup()
 
 void loop()
 {
-  Input = analogRead(PIN_INPUT);
+  Input = encoder.getLeftSpeed() - encoder.getRightSpeed();
   myPID.Compute();
-  analogWrite(PIN_OUTPUT, Output);
+  motor.leftForward(throttle + Output);
+  motor.rightForward(throttle - Output);
+
+  unsigned long currentTime = millis();
+  unsigned long elapsedTime = currentTime - lastTime;
+  
+  if(elapsedTime > 1000)
+  {
+    throttle += 5;
+    lastTime = currentTime;
+  }
+  
 }
---
+++ Ferros_pid_test/ferros_pid_test/motor.h
@@ -0,0 +1,23 @@
+
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <arduino.h>
+
+class Motor
+{
+public:
+    // Motor(int a1, int a1, int aSpd, int b1, int b2, int bSpd, int enable);
+    Motor();
+    void begin();
+    void enable();
+    void disable();
+    void stop();
+    void leftForward(int speed);
+    void leftBackward(int speed);
+    void rightForward(int speed);
+    void rightBackward(int speed);
+};
+
+
+#endif //MOTOR_H
