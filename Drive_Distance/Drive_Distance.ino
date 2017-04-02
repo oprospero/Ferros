a5532d81c2aa6c1b0cf145016f82d8e7c4ad0f27
Likhi <likhi.ondov@gmail.com>
2017-04-01 17:47:59 -0700

    created code to request distance to move via serial port.

--- Drive_Distance/Drive_Distance.ino
+++ Drive_Distance/Drive_Distance.ino
@@ -11,6 +11,12 @@ bool speedup;
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
+//Distance to drive
+int distance; // target distance to move in counts
+int moved; // number of counts moved
+float targetDistance; //target distance in inches
+float inchesToCount; // conversion factor from inches to count (e.g 2" = 100 counts)
+
 //Specify the links and initial tuning parameters
 double Kp = 2, Ki = 0, Kd = 0;
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
@@ -22,7 +28,7 @@ unsigned long lastTime = 0;
 
 void setup()
 {
-
+  encoder.resetCount();
   throttle = 50;
   // Po
   steeringTrim = -1.5;
@@ -30,6 +36,9 @@ void setup()
   encoder.begin();
   motor.begin();
   Serial.begin(115200);
+  distance = 0; // target distance
+  moved = 0; // encoder counts to track how far we moved
+  inchesToCount = (6.0/20.0);
 
   //initialize the variables we're linked to
   Input = 0;
@@ -42,53 +51,62 @@ void setup()
   Serial.println("Ready Player One");
 }
 
-void loop()
+void moveForward()
 {
-  encoder.poll();
-  float lspd = encoder.getLeftSpeed();
-  float rspd = encoder.getRightSpeed();
-  Input = lspd - rspd;
-  myPID.Compute();
-
-  if (throttle < 160)
-  {
-    motor.leftForward(0);
-    motor.rightForward(0);
-  }
-  else
-  {
-    motor.leftForward(throttle - Output);
-    motor.rightForward(throttle + Output);
-  }
-
-  unsigned long currentTime = millis();
-  unsigned long elapsedTime = currentTime - lastTime;
-
-  if (Serial.available())
-  {
-    int num = Serial.parseInt();
-    if (num < 1000)
-      throttle = (num);
-    else if ( num >= 1000)
-    {
-//      Setpoint = num / 1000;
-//      Kp = num / 1000;
-//      myPID.SetTunings(Kp, Ki, Kd);
-    }
-  }
-  if(elapsedTime > 200)
-  {
-//    Serial.print("T: "); Serial.println(throttle);
-//    Serial.print("lspd: "); Serial.println(lspd);
-//  Serial.print("rspd: "); Serial.println(rspd);
-  Serial.print("I: "); Serial.println(Input);
-  Serial.print("O: "); Serial.println(Output);
-    if (speedup) throttle += 5;
-//    else throttle -= 15;
-    if (throttle > 300) speedup = false;
-    if (throttle < 0) speedup = true;
-    lastTime = currentTime;
-  }
 
 }
 
+void loop()
+{
+  if (Serial.available())
+  {
+    if(Serial.parseFloat() != targetDistance)
+    {
+      distance = (int)(targetDistance * inchesToCount);
+      moved = 0;
+      encoder.resetCount();
+    } // this is now how many inches to move
+  }
+  while (moved < distance)
+  {
+    encoder.poll();
+    float lspd = encoder.getLeftSpeed();
+    float rspd = encoder.getRightSpeed(); // read this side
+
+    Input = lspd - rspd;
+    myPID.Compute();
+
+    if (throttle < 160)
+    {
+      motor.leftForward(0);
+      motor.rightForward(0);
+    }
+    else
+    {
+      motor.leftForward(throttle - Output);
+      motor.rightForward(throttle + Output);
+    }
+
+    unsigned long currentTime = millis();
+    unsigned long elapsedTime = currentTime - lastTime;
+
+
+    if(elapsedTime > 200)
+    {
+  //    Serial.print("T: "); Serial.println(throttle);
+  //    Serial.print("lspd: "); Serial.println(lspd);
+  //  Serial.print("rspd: "); Serial.println(rspd);
+    Serial.print("I: "); Serial.println(Input);
+    Serial.print("O: "); Serial.println(Output);
+      if (speedup) throttle += 5;
+  //    else throttle -= 15;
+      if (throttle > 300) speedup = false;
+      if (throttle < 0) speedup = true;
+      lastTime = currentTime;
+    }
+
+    moved += encoder.getLeftCount();
+
+  }
+
+}
