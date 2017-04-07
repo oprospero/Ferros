ea25e2953e99be198052a82f015370604261674a
Prosper <vanprosper@gmail.com>
2017-04-07 00:03:13 -0700

    Add new challenge folder and straight stuff

---
+++ Challenge/Straight/pindef.h
@@ -0,0 +1,27 @@
+
+#ifndef PINDEF_H
+#define PINDEF_H
+
+
+#define PIN_MOTOR1_IN1 4
+#define PIN_MOTOR1_IN2 12
+
+#define PIN_MOTOR2_IN1 7
+#define PIN_MOTOR2_IN2 8
+
+#define PIN_MOTOR1_SPD 5
+#define PIN_MOTOR2_SPD 6
+
+#define PIN_MOTOR_ENB 13
+
+#define PIN_ENCODER_1 2
+#define PIN_ENCODER_2 3
+
+#define PIN_GRIPPER_LIFT 9
+#define PIN_GRIPPER_CLAW 10
+
+#define PIN_SONAR_TRIG A0
+#define PIN_SONAR_ECHO A1
+
+
+#endif //PINDEF_H
